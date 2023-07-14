2ef045666ad619cb8d2431f8f9adce4ef4347faa
ISHU SRIVASTAV <Ishu9911039580@gmail.com>
2023-07-14 15:29:00 +0530

    This is my first project

---
+++ app/src/main/java/com/example/newsapp/Models/NewsApiResponse.java
@@ -0,0 +1,36 @@
+package com.example.newsapp.Models;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class NewsApiResponse implements Serializable {
+
+    String status;
+    String totalResults;
+    List<NewsHeadlines> articles;
+
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(String totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public List<NewsHeadlines> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(List<NewsHeadlines> articles) {
+        this.articles = articles;
+    }
+}
